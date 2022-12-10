5f8dc87c8323154bc1df778a6f5612d3793a4f79
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2022-12-10 20:45:06 +0100

    Adding Dunamoc Data And Undo in Dialogue Editor

--- Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -1,11 +1,15 @@
+using System;
 using UnityEditor;
 using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace RPG.Dialogue.Editor
 { 
 
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue = null;
+
         [MenuItem("Window/Dialogue Editor")]
         public static void ShowEditorWindow()
         {
@@ -22,5 +26,47 @@ namespace RPG.Dialogue.Editor
             }
             return false;
         }
+        private void OnEnable()
+        {
+            Selection.selectionChanged += onSelectionChanged;
+        }
+        private void OnDisable()
+        {
+            Selection.selectionChanged += onSelectionChanged;
+        }
+
+        private void OnGUI()
+        {
+            if (selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No dialogue selected.");
+            }
+            else
+            {
+                foreach (var node in selectedDialogue.GetAllNodes())
+                {
+                    EditorGUI.BeginChangeCheck();
+                    EditorGUILayout.LabelField("Node:");
+                    string id = EditorGUILayout.TextField(node.uniqueID);
+                    string testString = EditorGUILayout.TextField(node.text);
+                    
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        Undo.RecordObject(selectedDialogue, "Update Dialogue Text");
+                        node.uniqueID = id;
+                        node.text = testString;
+                    }
+                }
+            }
+        }
+        private void onSelectionChanged()
+        {
+            if (Selection.activeObject as Dialogue != null)
+            {
+                selectedDialogue = (Dialogue) Selection.activeObject;
+                Repaint();
+            }           
+        }
+
     }
 }
